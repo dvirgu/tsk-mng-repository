8489d4ed0744e2e13f1f3a6c5adae0e2e877277f
anara <anara@ANARA-LAP-WIN7.nice.com>
2012-08-25 19:49:28 +0300

    dvir fucked up

--- tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/service/TaskMangementBEService.java
+++ tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/service/TaskMangementBEService.java
@@ -5,13 +5,41 @@ import org.tsk.mng.backend.model.UserBE;
 
 public interface TaskMangementBEService {
 
+	/**
+	 * 
+	 * @param user
+	 * @param task
+	 * @return
+	 */
 	UserBE assignUserToTask(UserBE user, TaskBE task);
 	
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 */
 	TaskBE getUserTasks(UserBE user);
 	
+	/**
+	 * 
+	 * @param user
+	 * @param task
+	 * @return
+	 */
 	TaskBE removeTaskFromUser(UserBE user, TaskBE task);
 	
+	/**
+	 * 
+	 * @param task
+	 * @return
+	 */
 	TaskBE updateTask(TaskBE task);
 	
+	/**
+	 * 
+	 * @param task
+	 * @param dependentTask
+	 * @return
+	 */
 	UserBE addDependencyToTask(TaskBE task, TaskBE dependentTask);
 }
