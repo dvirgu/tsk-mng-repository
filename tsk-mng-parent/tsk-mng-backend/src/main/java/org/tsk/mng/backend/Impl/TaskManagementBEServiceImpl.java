a711e72fa873fbc125d879c24cf6239949314ee0
anara <anara@ANARA-LAP-WIN7.nice.com>
2012-08-25 19:56:35 +0300

    dvir fucked up second time

--- tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/Impl/TaskManagementBEServiceImpl.java
+++ tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/Impl/TaskManagementBEServiceImpl.java
@@ -1,5 +1,8 @@
 package org.tsk.mng.backend.Impl;
 
+import java.util.Set;
+
+import org.tsk.mng.backend.exceptions.OperationFailureException;
 import org.tsk.mng.backend.model.TaskBE;
 import org.tsk.mng.backend.model.UserBE;
 import org.tsk.mng.backend.service.TaskMangementBEService;
@@ -14,32 +17,32 @@ public class TaskManagementBEServiceImpl implements TaskMangementBEService {
 		return taskDao;
 	}
 
-	public void setTaskDao(TaskDao taskDao) {
+	public void setTaskDao(TaskDao taskDao) throws OperationFailureException {
 		this.taskDao = taskDao;
 	}
 
 	
-	public UserBE assignUserToTask(UserBE user, TaskBE task) {
+	public UserBE assignUserToTask(UserBE user, TaskBE task) throws OperationFailureException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public TaskBE getUserTasks(UserBE user) {
+	public Set<TaskBE> getUserTasks(UserBE user) throws OperationFailureException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public TaskBE removeTaskFromUser(UserBE user, TaskBE task) {
+	public TaskBE removeTaskFromUser(UserBE user, TaskBE task) throws OperationFailureException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public TaskBE updateTask(TaskBE task) {
+	public TaskBE updateTask(TaskBE task) throws OperationFailureException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public UserBE addDependencyToTask(TaskBE task, TaskBE dependentTask) {
+	public UserBE addDependencyToTask(TaskBE task, TaskBE dependentTask) throws OperationFailureException {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/service/TaskMangementBEService.java
+++ tsk-mng-parent/tsk-mng-backend/src/main/java/org/tsk/mng/backend/service/TaskMangementBEService.java
@@ -1,5 +1,8 @@
 package org.tsk.mng.backend.service;
 
+import java.util.Set;
+
+import org.tsk.mng.backend.exceptions.OperationFailureException;
 import org.tsk.mng.backend.model.TaskBE;
 import org.tsk.mng.backend.model.UserBE;
 
@@ -10,36 +13,42 @@ public interface TaskMangementBEService {
 	 * @param user
 	 * @param task
 	 * @return
+	 * @throws OperationFailureException
 	 */
-	UserBE assignUserToTask(UserBE user, TaskBE task);
+	UserBE assignUserToTask(UserBE user, TaskBE task) throws OperationFailureException;
 	
 	/**
 	 * 
 	 * @param user
-	 * @return
+	 * @return 
+	 * @throws OperationFailureException
 	 */
-	TaskBE getUserTasks(UserBE user);
+	Set<TaskBE> getUserTasks(UserBE user) throws OperationFailureException;
 	
 	/**
+	 * Validates if the task exist and if exists then remove the task, otherwise threw exception.
 	 * 
 	 * @param user
 	 * @param task
-	 * @return
+	 * @return Removed task
+	 * @throws OperationFailureException
 	 */
-	TaskBE removeTaskFromUser(UserBE user, TaskBE task);
+	TaskBE removeTaskFromUser(UserBE user, TaskBE task) throws OperationFailureException;
 	
 	/**
 	 * 
 	 * @param task
-	 * @return
+	 * @return Updated task
+	 * @throws OperationFailureException
 	 */
-	TaskBE updateTask(TaskBE task);
+	TaskBE updateTask(TaskBE task) throws OperationFailureException;
 	
 	/**
 	 * 
 	 * @param task
 	 * @param dependentTask
 	 * @return
+	 * @throws OperationFailureException
 	 */
-	UserBE addDependencyToTask(TaskBE task, TaskBE dependentTask);
+	UserBE addDependencyToTask(TaskBE task, TaskBE dependentTask) throws OperationFailureException;
 }
